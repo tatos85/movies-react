d033dee4a77664b4f4c2e5b657c98706760638fc
tatos85 <riverac85@gmail.com>
2022-06-24 16:06:05 -0500

    Rutas y nueva vista blog

--- src/App.js
+++ src/App.js
@@ -1,86 +1,22 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Pelicula from './Pelicula';
-import PageWrapper from './PageWrapper';
-import Paginacion from './Paginacion';
-import { useEffect, useState } from 'react';
+import Blog from './views/Blog';
+import ListadoPeliculas from './views/ListadoPeliculas';
 
 function App() {
-  const [paginaActual, setPaginaActual] = useState(1);
-  const [peliculas, setPeliculas] = useState([]);
-  const TOTAL_POR_PAGINA = 4;
-
-  useEffect(()=>{
-    buscarPeliculas();
-  }, [])
-
-
-  const buscarPeliculas = async () => {
-    let url = "https://corsanywhere.herokuapp.com/https://lucasmoy.dev/data/react/peliculas.json";
-
-
-    // fetch(url, {
-    //   "method":"GET",
-    //   "headers":{
-    //     "Accept":"application/json",
-    //     "Content-Type":"application/json"
-    //   }
-    // }).then(result => {
-      
-    // })
-
-    let result = await  fetch(url, {
-          "method":"GET",
-        
-          "headers":{
-            "Accept":"application/json",
-            "Content-Type":"application/json",
-            "Origin":"https://lucasmoy.dev/",
-            "Access-Control-Allow-Origin" : "*", 
-"Access-Control-Allow-Credentials" : true 
-          }
-        })
-
-    let json = await result.json();
-
-    setPeliculas(json);
-
-  }
-
-
-  const cargarPeliculas = () => {
-  }
-
-  const getTotalPaginas = () => {
-    let cantidadTotal = peliculas.length;
-    return Math.ceil(cantidadTotal/TOTAL_POR_PAGINA);
-     
-  }
-
-
-  let peliculasPorPagina = peliculas.slice((paginaActual-1) * TOTAL_POR_PAGINA, paginaActual * TOTAL_POR_PAGINA);
-
 
   return (
-    <PageWrapper>
-            {
-              peliculasPorPagina.map(pelicula => 
-                             
-                <Pelicula  titulo={pelicula.titulo} calificacion={pelicula.calificacion}
-                  director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion}
-                  img={pelicula.img}
-                >
-                  {pelicula.description}
-                </Pelicula>
-              )
-            }
+    <BrowserRouter>
+       <Routes>
 
-            <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina)=>{
-              setPaginaActual(pagina);
-            }}>
+      <Route path="/" element={<ListadoPeliculas></ListadoPeliculas>}/> 
+      <Route path="/blog" element={ <Blog></Blog>} />
+      
+    </Routes> 
+    </BrowserRouter>
 
-            </Paginacion>
-    </PageWrapper>
+
   )
 }
 
--- src/views/PageWrapper.js
+++ src/views/PageWrapper.js
@@ -108,31 +108,14 @@ export default function PageWrapper(props){
                                 <a href="#page-top"></a>
                             </li>
                             <li className="dropdown first">
-                                <a className="btn btn-default dropdown-toggle lv1" data-toggle="dropdown">
-                                Home <i className="fa fa-angle-down" aria-hidden="true"></i>
+                                <a href="/" className="btn btn-default dropdown-toggle lv1">
+                                HOME
                                 </a>
-                                <ul className="dropdown-menu level1">
-                                    <li><a href="index-2.html">Home 01</a></li>
-                                    <li><a href="homev2.html">Home 02</a></li>
-                                    <li><a href="homev3.html">Home 03</a></li>
-                                </ul>
                             </li>
                             <li className="dropdown first">
-                                <a className="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
-                                movies<i className="fa fa-angle-down" aria-hidden="true"></i>
+                                <a href="/blog" className="btn btn-default dropdown-toggle lv1">
+                                BLOG
                                 </a>
-                                <ul className="dropdown-menu level1">
-                                    <li className="dropdown">
-                                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" >Movie grid<i className="ion-ios-arrow-forward"></i></a>
-                                        <ul className="dropdown-menu level2">
-                                            <li><a href="moviegrid.html">Movie grid</a></li>
-                                            <li><a href="moviegridfw.html">movie grid full width</a></li>
-                                        </ul>
-                                    </li>			
-                                    <li><a href="movielist.html">Movie list</a></li>
-                                    <li><a href="moviesingle.html">Movie single</a></li>
-                                    <li className="it-last"><a href="seriessingle.html">Series single</a></li>
-                                </ul>
                             </li>
                             <li className="dropdown first">
                                 <a className="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
